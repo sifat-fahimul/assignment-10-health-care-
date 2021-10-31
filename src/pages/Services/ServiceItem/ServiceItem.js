d362a1d0621dd8dd403c9d8c29da69c9b58cc3d4
sifat-fahimul <mdsifatfeni@gmail.com>
2021-10-31 22:24:22 +0600

    commit

--- src/pages/Services/ServiceItem/ServiceItem.js
+++ src/pages/Services/ServiceItem/ServiceItem.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 
 const ServiceItem = () => {
     const { serviceId } = useParams();
+    const [services, setServices] = useState([]);
+    const [matchService, setMatchService] = useState({})
+
+    useEffect(() => {
+        fetch('/service.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
+    useEffect(() => {
+        if (services.length > 0) {
+            const matchData = services.find(service => service.id === serviceId);
+            setMatchService(matchData);
+        }
+    }, [services])
+
+
+
     return (
         <div className='text-center container my-5 py-5'>
-            <h1>this is service item {serviceId}</h1>
+            <h2> {matchService?.title}</h2>
+            <img className='w-75' src={matchService?.img} alt="" />
+            <p>{matchService?.date}</p>
+            <h3>Details : {matchService?.details}</h3>
             <Link to='/'>
                 <button className='btn btn-outline-danger rounded-pill text-dark px-3 fw-bold'>Go To Home</button>
             </Link>
